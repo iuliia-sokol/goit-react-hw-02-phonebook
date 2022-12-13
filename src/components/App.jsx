8c43dee756c3520413905c471dda5ad29c442526
Iuliia Sokol <avare779@gmail.com>
2022-12-13 08:31:24 +0200

    removed useless elements from app state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,8 +25,6 @@ export class App extends React.Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
   onAddBtnClick = FormData => {
